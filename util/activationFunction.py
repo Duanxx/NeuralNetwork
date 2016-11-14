c005c875976946e8aaf96701cf59ed17c23b381b
Duanxx <duanxxnj@163.com>
2016-11-14 15:42:00 +0800

    add activationFunction

--- util/activationFunction.py
+++ util/activationFunction.py
@@ -9,15 +9,44 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plot_sigmoid_function():
-    x = np.arange(-10, 10, 0.1)
-    y = 1/(1 + np.exp(-x))
+# sigmoid function
+# input :x array like
+# return 1/(1+exp(x))
+def sigmoid(x):
+    x = np.array(x)
+    y = 1.0/(1.0 + np.exp(-x))
+    return y
+
+
+# sigmoid function
+# input :x array like
+# return ln(1 + exp(x))
+def rectifier(x):
+    x = np.array(x)
+    y = np.log(1 + np.exp(x))
+    return y
+
+
+# plot the activation function
+def plot_activation_function(activation_func, xr=[-5, 5]):
+    x = np.arange(xr[0], xr[1], 0.1)
+    y = activation_func(x)
     plt.plot(x, y)
-    plt.xlim(-10.5, 10.5)
-    plt.ylim(-0.1, 1.1)
+    plt.plot(x*0, x, c='r')
+    plt.plot(x, x*0, c='r')
+    plt.xlim(x.min() - 0.1, x.max() + 0.1)
+    plt.ylim(y.min() - 0.1, y.max() + 0.1)
     plt.grid()
-    plt.show()
 
 
 if __name__ == '__main__':
-    plot_sigmoid_function()
+    plt.figure(1)
+    plot_activation_function(sigmoid)
+
+    plt.figure(2)
+    plot_activation_function(np.tanh)
+
+    plt.figure(3)
+    plot_activation_function(rectifier)
+
+    plt.show()
